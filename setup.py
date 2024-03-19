8b8f255d72140f774301f5c05dae2618db130320
xxxxxa-hub <3291275159@qq.com>
2024-03-19 11:16:52 -0400

    Init

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ setup(
     version="0.1",
     packages=find_packages(),
     install_requires=[
-        "gym==0.23.0",
-        "mujoco-py==2.1.2.14",
-        "tqdm==4.66.1",
-        "pandas==2.0.3",
         "numpy==1.21.5",
+        "gym==0.23.0",
+        "h5py==3.1.0",
+        "mujoco-py==2.1.2.14",
+        "pandas==2.0.3",
+        "tqdm==4.66.1",
         "tensorflow==2.6.0",
         "tensorflow-addons==0.16.1",
         "tensorflow-probability==0.14.1",
-        "h5py==3.1.0",
         "typing-extensions==3.7.4.3",
         "tf-agents==0.9.0"
     ]
